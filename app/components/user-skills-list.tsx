12e047f3e1879306b1edf119412880154f5b442c
ericnoharaleclair <eric.noharaleclair@gmail.com>
2024-11-04 16:45:53 -0500

    Added delete component to delete skills using our API

--- app/components/user-skills-list.tsx
+++ app/components/user-skills-list.tsx
@@ -1,6 +1,6 @@
 "use client";
-import React, { useState } from "react";
-import useSWR from "swr";
+import React from "react";
+import useSWR, { mutate } from "swr";
 import {
   List,
   ListItem,
@@ -11,7 +11,7 @@ import {
 } from "@mui/material";
 import DeleteIcon from "@mui/icons-material/Delete";
 
-const fetcher = async (url: string) => {
+const getSkillsFetcher = async () => {
   try {
     const token: string | null = localStorage.getItem("token");
 
@@ -35,8 +35,32 @@ const fetcher = async (url: string) => {
   }
 };
 
+const deleteSkillFetcher = async (skillName: string) => {
+  try {
+    const token: string | null = localStorage.getItem("token");
+
+    const res = await fetch(`/api/skill/${skillName}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      alert(`Error: ${data.message}`);
+    } else {
+      alert(data.message);
+    }
+  } catch (err) {
+    console.error(err);
+  }
+};
+
 export default function UserSkillsList() {
-  const { data: skills, error } = useSWR("/api/skill", fetcher);
+  const { data: skills, error } = useSWR("/api/skill", getSkillsFetcher);
 
   if (error) return <div>Error: {error.message}</div>;
   if (!skills)
@@ -51,6 +75,15 @@ export default function UserSkillsList() {
       </Box>
     );
 
+  const handleDeleteSkill = async (skillName: string) => {
+    try {
+      await deleteSkillFetcher(skillName);
+      mutate("/api/skill"); // used to update the fetched data by useSWR
+    } catch (err) {
+      console.error("Failed to delete skill:", err);
+    }
+  };
+
   return (
     <List>
       {skills.map((skill: string, index: number) => (
@@ -62,7 +95,7 @@ export default function UserSkillsList() {
             alignItems="center"
           >
             <ListItemText>{skill}</ListItemText>
-            <ListItemButton>
+            <ListItemButton onClick={() => handleDeleteSkill(skill)}>
               <DeleteIcon />
             </ListItemButton>
           </Box>
